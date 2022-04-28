b0d4274643e3d5ef5792a49038077f8cc41f0c82
Robert Seffens <rseffens@gmail.com>
2022-04-28 13:58:14 -0700

    Loops

---
+++ loop.js
@@ -0,0 +1,36 @@
+// Print odds 1-20
+
+// for(var i=1; i<20; i+=2) {
+//     console.log(i)
+// }
+
+// decreasing multiples of 3
+
+// for(var i=100; i>=0; i--) {
+//     if(i % 3 == 0) {
+//         console.log(i)
+//     }
+// }
+
+// Print the sequence
+// Using a loop write code that will console.log the values in this sequence 4, 2.5, 1, -0.5, -2, -3.5.
+
+// for(var i=4; i>-4; i-=1.5){
+//     console.log(i)
+// }
+
+// Sigma
+// Write code that will add all of the values from 1-100 onto some variable sum and at the end console.log the result 1 + 2 + 3 + ... + 98 + 99 + 100. We should get back 5050 at the end.
+
+// var sum = 0;
+// for(i=1; i<=100; i++) {
+//     sum +=i;
+// }
+// console.log(sum);
+
+var product = 1
+for(i=1; i<=12; i++){
+    product *=i;
+}
+
+console.log(product)
